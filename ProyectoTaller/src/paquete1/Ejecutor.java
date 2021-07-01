847a8adc5ab0bee1cb9b13f0648e48c8106c7610
mapaute2 <73542451+mapaute2@users.noreply.github.com>
2021-07-01 17:45:12 -0500

    Taller2

--- ProyectoTaller/src/paquete1/Ejecutor.java
+++ ProyectoTaller/src/paquete1/Ejecutor.java
@@ -12,6 +12,11 @@ import paquete3.*;
 public class Ejecutor {
 
     public static void main(String[] args) {
+        
+        /*
+        Integrantes: Jose Criollo
+                     Alejandra Paute
+        */
 
         // nombre del archivo
         String nombreArchivo = "pasajes.data";
